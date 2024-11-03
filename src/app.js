574939fa2535a9e9783ee86a60ea05d9c1fcd0a0
stella njeri ndanuko <sndanuko@gmail.com>
2024-11-03 13:50:58 +0300

    Added weather api

--- src/app.js
+++ src/app.js
@@ -1,10 +1,22 @@
+function showWeather(response) {
+  let tempElement = document.querySelector("#temp");
+  let cityInputElement = document.querySelector("#city-input");
+  cityInputElement.innerHTML = response.data.city;
+  tempElement.innerHTML = Math.round(response.data.temperature.current);
+  console.log(response.data.temperature.current);
+}
+function searchCity(city) {
+  let apiKey = "537fcbc844832atbo347402443444238";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(showWeather);
+}
 function handleSearchSubmit(event) {
   event.preventDefault();
   console.log(handleSearchSubmit);
   let searchInput = document.querySelector("#search-form-input");
-  let cityInputElement = document.querySelector("#city-input");
-  cityInputElement.innerHTML = searchInput.value;
-  console.log(searchInput.value);
+
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity(Nairobi);
